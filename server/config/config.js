43f01266b29b44ab5946e4e7692611fd3abd7752
skye <full.skyl@hotmail.com>
2024-04-17 18:47:13 -0600

    Update MongoDB connection configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,11 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI)
+    } catch (err) {
+        console.log(`A MongoDB connection or specific mongodb Error has occurred: ${err.message}`);
+    }
+}
+
+module.exports = connectDB;
